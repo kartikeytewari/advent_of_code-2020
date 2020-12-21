ea7e07b27c33ef4bf1630dc82990e090d9538406
Kartikey Tewari <kartikeya30@gmail.com>
2020-12-21 16:52:37 +0530

    Added day-18 solutions Solved without parser, and changed operator precedence using custom class And some typecasting

---
+++ 18/2.py
@@ -0,0 +1,21 @@
+import re
+file = open("in","r")
+
+class num(int):
+    def __sub__ (self, a):
+        return num(int(self)*int(a))
+
+    def __mul__ (self, a):
+        return num(int(self)+int(a))
+
+
+ans=0
+for i in file:
+    i=i.strip('\n')
+    i=i.replace('*','-')
+    i=i.replace('+','*')
+    i=re.sub('(\d+)', r'num(\1)', i)
+    print(i)
+    ans+=eval(i)
+
+print(ans)
